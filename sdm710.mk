d803152ec2bef1cfced1a8d67e7d4c4c02e64c6e
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    sdm710-common: Drop no longer existed BluetoothQti
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84

--- sdm710.mk
+++ sdm710.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
-    BluetoothQti \
     libldacBT_dec \
     libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
