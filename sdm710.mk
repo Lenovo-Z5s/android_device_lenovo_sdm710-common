f51c745845c71e8871aa25212515e88abc72f711
Alex Hong <rurumihong@google.com>
2023-01-06 20:16:58 +0800

    sdm710-common: device: Replace the HIDL with AIDL for android.hardware.memtrack
    The HIDL interface of "android.hardware.memtrack" is no longer supported
    for Android S.
    
    Test: atest VtsHalMemtrackTargetTest pass
    Bug: 186539435
    Change-Id: Iaafd526fc5a601f7e8189ec764399d99c660beb0

--- sdm710.mk
+++ sdm710.mk
@@ -161,8 +161,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.example \
     gralloc.sdm710 \
     hwcomposer.sdm710 \
     libdisplayconfig.qti \
