ade32dd8dda6409c7b1ee943db97e8d16a35c0e5
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-19 14:13:37 +0000

    sdm710-common: Setup Retrofit Dynamic Partitions

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,3 +7,12 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
--- sdm710.mk
+++ sdm710.mk
@@ -126,6 +126,7 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
+    fstab.qcom_ramdisk \
     fstab.qcom \
     init.class_main.sh \
     init.msm.usb.configfs.rc \
@@ -225,6 +226,11 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
+
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
 # FM
 PRODUCT_PACKAGES += \
