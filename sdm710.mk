5c2172df6325bf6fce930db663a6af48b2173368
Mathieu Chartier <mathieuc@google.com>
2023-03-29 15:56:41 +0800

    sdm710-common: device: Speed profile services and wifi-service to reduce RAM and storage

--- sdm710.mk
+++ sdm710.mk
@@ -402,6 +402,9 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys/system/bt/conf \
     vendor/qcom/opensource/usb/etc
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
