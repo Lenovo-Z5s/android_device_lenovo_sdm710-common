bf630c3a2331fe30d72598af8f2abb41a91ad46b
CakesTwix <oleg.kiryazov@gmail.com>
2023-06-23 23:29:30 +0300

    sdm710-common: Drop android.hardware.power.stats@1.0-service.lenovo

--- sdm710.mk
+++ sdm710.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.lenovo-sdm710-libperfmgr \
-    android.hardware.power.stats@1.0-service.lenovo
+    android.hardware.power-service.lenovo-sdm710-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
