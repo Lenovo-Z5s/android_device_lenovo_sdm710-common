581430957948f0199752747d50c661183176207b
Noah <kingfighter2958@gmail.com>
2023-08-25 13:37:56 +0530

    sdm710-common: Build Aperture camera Signed-off-by: Noah113-Allen <kingfighter2958@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -115,8 +115,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor
-
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    Aperture
+    
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend
