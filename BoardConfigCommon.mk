49ae0babc90c3c7ef0ceb6e64b3d03a18ecd7472
CakesTwix <oleg.kiryazov@gmail.com>
2025-05-01 11:34:16 +0300

    sdm710-common: Added DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,10 @@ BOARD_HAS_QCA_FM_SOC := "cherokee"
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
